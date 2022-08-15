b86502a0eb08a92290dcb9ce6f0ff180364fc4d6
Henryiscoding <103418360+Henryiscoding@users.noreply.github.com>
2022-08-15 14:00:58 +0200

    first

---
+++ shop.php
@@ -0,0 +1,170 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:login.php');
+};
+
+if(isset($_POST['add_to_wishlist'])){
+
+   $pid = $_POST['pid'];
+   $pid = filter_var($pid, FILTER_SANITIZE_STRING);
+   $p_name = $_POST['p_name'];
+   $p_name = filter_var($p_name, FILTER_SANITIZE_STRING);
+   $p_price = $_POST['p_price'];
+   $p_price = filter_var($p_price, FILTER_SANITIZE_STRING);
+   $p_image = $_POST['p_image'];
+   $p_image = filter_var($p_image, FILTER_SANITIZE_STRING);
+
+   $check_wishlist_numbers = $conn->prepare("SELECT * FROM `wishlist` WHERE name = ? AND user_id = ?");
+   $check_wishlist_numbers->execute([$p_name, $user_id]);
+   echo json_encode($check_wishlist_numbers);
+
+   $check_cart_numbers = $conn->prepare("SELECT * FROM `cart` WHERE name = ? AND user_id = ?");
+   $check_cart_numbers->execute([$p_name, $user_id]);
+   echo json_encode($check_cart_numbers);
+
+   if($check_wishlist_numbers->rowCount() > 0){
+      $message[] = 'already added to wishlist!';
+   }elseif($check_cart_numbers->rowCount() > 0){
+      $message[] = 'already added to cart!';
+   }else{
+      $insert_wishlist = $conn->prepare("INSERT INTO `wishlist`(user_id, pid, name, price, image) VALUES(?,?,?,?,?)");
+      $insert_wishlist->execute([$user_id, $pid, $p_name, $p_price, $p_image]);
+      print_r(json_encode($insert_wishlist));
+      $message[] = 'added to wishlist!';
+   }
+
+}
+
+if(isset($_POST['add_to_cart'])){
+
+   $pid = $_POST['pid'];
+   $pid = filter_var($pid, FILTER_SANITIZE_STRING);
+   $p_name = $_POST['p_name'];
+   $p_name = filter_var($p_name, FILTER_SANITIZE_STRING);
+   $p_price = $_POST['p_price'];
+   $p_price = filter_var($p_price, FILTER_SANITIZE_STRING);
+   $p_image = $_POST['p_image'];
+   $p_image = filter_var($p_image, FILTER_SANITIZE_STRING);
+   $p_qty = $_POST['p_qty'];
+   $p_qty = filter_var($p_qty, FILTER_SANITIZE_STRING);
+
+   $check_cart_numbers = $conn->prepare("SELECT * FROM `cart` WHERE name = ? AND user_id = ?");
+   $check_cart_numbers->execute([$p_name, $user_id]);
+   echo json_encode($check_cart_numbers);
+
+   if($check_cart_numbers->rowCount() > 0){
+      $message[] = 'already added to cart!';
+   }else{
+
+      $check_wishlist_numbers = $conn->prepare("SELECT * FROM `wishlist` WHERE name = ? AND user_id = ?");
+      $check_wishlist_numbers->execute([$p_name, $user_id]);
+      echo json_encode($check_wishlist_numbers);
+
+      if($check_wishlist_numbers->rowCount() > 0){
+         $delete_wishlist = $conn->prepare("DELETE FROM `wishlist` WHERE name = ? AND user_id = ?");
+         $delete_wishlist->execute([$p_name, $user_id]);
+         print_r(json_encode($delete_wishlist));
+      }
+
+      $insert_cart = $conn->prepare("INSERT INTO `cart`(user_id, pid, name, price, quantity, image) VALUES(?,?,?,?,?,?)");
+      $insert_cart->execute([$user_id, $pid, $p_name, $p_price, $p_qty, $p_image]);
+      print_r(json_encode($insert_cart));
+      $message[] = 'added to cart!';
+   }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>shop</title>
+
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'header.php'; ?>
+
+<section class="p-category">
+
+   <a href="category.php?category=Fruit and Veg">Fruit and Veg</a>
+   <a href="category.php?category=Dairy, Milk and Eggs">Dairy, Milk and Eggs</a>
+   <a href="category.php?category=Meats">Meats</a>
+   <a href="category.php?category=Bread">Bread</a>
+   <a href="category.php?category=Beverages">Beverages</a>
+   <a href="category.php?category=Desserts">Desserts</a>
+   <a href="category.php?category=Cereal">Cereal</a>
+   <a href="category.php?category=Hygiene">Hygiene</a>
+   <a href="category.php?category=Grains">Grains</a>
+   <a href="category.php?category=Health">Health</a>
+   <a href="category.php?category=Sweets and Chocolate">Sweets and Chocolate</a>
+   <a href="category.php?category=Hot drinks">Hot drinks</a>
+   <a href="category.php?category=Biscuits and Rusks">Biscuits and Rusks</a>
+
+</section>
+
+<section class="products">
+
+   <h1 class="title">latest products</h1>
+
+   <div class="box-container">
+
+   <?php
+      $select_products = $conn->prepare("SELECT * FROM `products`");
+      $select_products->execute();
+      if($select_products->rowCount() > 0){
+         while($fetch_products = $select_products->fetch(PDO::FETCH_ASSOC)){ 
+            echo json_encode($fetch_products);
+   ?>
+   <form action="" class="box" method="POST">
+      <div class="price">R<span><?= $fetch_products['price']; ?></span></div>
+      <a href="view_page.php?pid=<?= $fetch_products['id']; ?>" class="fas fa-eye"></a>
+      <img src="uploaded_img/shop_images/<?= $fetch_products['image']; ?>" alt="">
+      <div class="name"><?= $fetch_products['name']; ?></div>
+      <input type="hidden" name="pid" value="<?= $fetch_products['id']; ?>">
+      <input type="hidden" name="p_name" value="<?= $fetch_products['name']; ?>">
+      <input type="hidden" name="p_price" value="<?= $fetch_products['price']; ?>">
+      <input type="hidden" name="p_image" value="<?= $fetch_products['image']; ?>">
+      <input type="number" min="1" value="1" name="p_qty" class="qty">
+      <input type="submit" value="add to wishlist" class="option-btn" name="add_to_wishlist">
+      <input type="submit" value="add to cart" class="btn" name="add_to_cart">
+   </form>
+   <?php
+      }
+   }else{
+      echo '<p class="empty">no products added yet!</p>';
+   }
+   ?>
+
+   </div>
+
+</section>
+
+
+
+
+
+
+
+
+<?php include 'footer.php'; ?>
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
